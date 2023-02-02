68a0be3e00c17166bb9a2aa5ed6f28e70d16354e
borisbagz <boris@gmail.com>
2023-02-02 17:08:19 -0500

    fix hopefully

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
 import requests
+from bs4 import BeautifulSoup
+
 
 URL = "https://www.pricesmart.com/site/pa/es/pagina-producto/956696"
 page = requests.get(URL, verify=False)
 
-print(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="inventory-id")
