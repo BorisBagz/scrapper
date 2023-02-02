5214d31a8a0618a58a8c03bfd017015072419658
borisbagz <boris@gmail.com>
2023-02-02 16:51:10 -0500

    fix hopefully

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python
+
 import urllib.request
+
 with urllib.request.urlopen('https://www.pricesmart.com/site/pa/es/pagina-producto/956696') as response:
    html = response.read()
    html_decoded = html.decode("utf-8")
